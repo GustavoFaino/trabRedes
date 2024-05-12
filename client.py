45d3783c208cc43bdfd231f1acd446d4a6eb5844
GustavoFaino <gustavomfaino@gmail.com>
2024-05-11 21:46:55 -0300

    criar/listar salas

--- client.py
+++ client.py
@@ -1,37 +1,85 @@
+import os
 import socket
 import threading
+
+def clear():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
 
 def receive():
     while True:
         try:
             message = client.recv(1024).decode('utf-8')
-            if message == 'NICK':
-                client.send(nickname.encode('utf-8'))
-            else:
-                print(message)
+            print(message)
         except:
             print("Erro!")
             client.close()
             break
 
+
 def write():
     while True:
-        message = f'{nickname}: {input("")}'
+        message = input("")
         client.send(message.encode('utf-8'))
 
 
+def registro():
+    message = f'REGISTRO {username}'
+    client.send(message.encode('utf-8'))
+
+    while True:
+        response = client.recv(1024).decode('utf-8') # cuidar esta linha 
+        split_res = response.split(' ')
+        
+
+        if(split_res[0] == 'REGISTRO_OK'):
+            print("Registro feito com sucesso!")
+            return
+        elif(split_res[0] == 'ERRO'):
+            print(response)
+            break
+
+
+""" def createChat():
+    print("create")
+    c = input()
+    
+# joinChat:
+
+
+def menu():
+    choice = 'a'
+
+    while choice != '0':
+
+        print("[0] Sair")
+        print("[1] Entrar Sala")
+        print("[2] Criar em Sala")
+
+        choice = input()
+
+        match choice:
+            case '1':
+                print("entrar sala")
+            case '2':
+                createChat()
+        clear() """
+            
 
 ip = input("Digite o IP do server: ")
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect((str(ip), 12345))
 
-nickname = input("Digite o seu nickname: ")
-
-
+username = input("Digite o seu username: ")
+registro()
 
 receive_thread = threading.Thread(target=receive)
 receive_thread.start()
 
 write_thread = threading.Thread(target=write)
 write_thread.start()
+
+
+
+
 
--- server.py
+++ server.py
@@ -18,14 +18,20 @@ def get_host_ip():
 
 
 
+def salaExiste(nome, salaList):
+    for sala in salaList:
+        if nome == sala.nome:
+            return True
+    return False
+
+
 class Sala:
-    def __init__(self, nome, admin):
-        self.nome = nome
+    def __init__(self):
+        self.nome = ''
         self.senha = ''
-        self.admin = admin
+        self.admin = ''
         self.banned = []
         self.clients = []
-        clients.append(admin)
 
 
 
@@ -34,55 +40,142 @@ class Servidor:
         self.host = host
         self.port = port
         self.server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.server.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
         self.server.bind((self.host, self.port))
         self.server.listen()
         self.clients = []
-        self.nicknames = []
+        self.usernames = []
         self.salas = []
+
+
 
     def broadcast(self, message):
         for client in self.clients:
             client.send(message)
 
+
+
+    def criarSala(self, message, client):        
+
+        split_msg = message.split(' ')
+        
+        if(len(split_msg) < 3):
+            client.send('ERRO Mensagem falta informações'.encode('utf-8'))
+            return    
+
+        salaAux = Sala()
+
+        senha = ''
+        nome = split_msg[2]
+        tipo = split_msg[1]
+
+        if tipo == 'PRIVADA':
+            if len(split_msg) < 4:
+                client.send('ERRO Sala privada deve ter senha'.encode('utf-8'))
+                return   
+            salaAux.senha = senha
+
+        if salaExiste(nome, self.salas):  
+            client.send('ERRO Já existe uma sala com esse nome'.encode('utf-8')) 
+            return
+
+        elif((len(split_msg) > 3 and tipo == 'PUBLICA') or (len(split_msg) > 4 and tipo == 'PRIVADA')):
+            client.send('ERRO Nomes não podem ter espaços'.encode('utf-8'))
+            return
+        
+        salaAux.nome = nome
+        salaAux.admin = client
+
+        self.salas.append(salaAux)
+
+        client.send('CRIAR_SALA_OK'.encode('utf-8'))
+
+
+    def listarSalas(self, client):
+        mensagem = 'SALAS'
+
+        for sala in self.salas:
+            mensagem = mensagem + ' ' + sala.nome
+            if sala.senha == '':
+                mensagem = mensagem + '[publica]'
+            else:
+                mensagem = mensagem + '[privada]'
+        
+        client.send(mensagem.encode('utf-8'))
+
+
+    def treat_message(self, message, client):
+        split_msg = message.split(' ') # separa a mensagem de acordo com espacos
+        command = split_msg[0]
+
+        match command:
+            case 'CRIAR_SALA':
+                print('criar sala')
+                self.criarSala(message, client)
+            case 'LISTAR_SALAS':
+                print("listar salas")
+                self.listarSalas(client)
+                
+        
+
+
     def handle(self, client):
         while True:
             try:
-                message = client.recv(1024)
-                self.broadcast(message)
+                message = client.recv(1024).decode('utf-8')
+                self.treat_message(message, client)
+                #self.broadcast(message)
             except:
                 index = self.clients.index(client)
                 self.clients.remove(client)
                 client.close()
-                nickname = self.nicknames[index]
-                self.nicknames.remove(nickname)
-                self.broadcast(f'{nickname} saiu do chat!'.encode('utf-8'))
+                username = self.usernames[index]
+                self.usernames.remove(username)
+                self.broadcast(f'{username} saiu do chat!'.encode('utf-8'))
                 break
 
-    def receive(self):
+
+
+    def register(self):
         while True:
             client, address = self.server.accept()
             print(f"Conectado com {str(address)}")
-
             
-            client.send('NICK'.encode('utf-8'))
-            nickname = client.recv(1024).decode('utf-8')
-
-            if((nickname not in self.nicknames) and (" " not in nickname)):  
-                self.nicknames.append(nickname)
-                self.clients.append(client)
-
-                print(f"Nickname do cliente é {nickname}!")
-                self.broadcast(f"{nickname} entrou no chat!".encode('utf-8'))
-                client.send('Conectado ao servidor!'.encode('utf-8'))
-
-                thread = threading.Thread(target=self.handle, args=(client,))
-                thread.start()
+            message = client.recv(1024).decode('utf-8')
             
-            else:
-                client.send("Erro, nickname inválido".encode('utf-8'))  
+            split_msg = message.split(' ') # separa a mensagem de acordo com espacos
+            command = split_msg[0]
+
+            if(command == 'REGISTRO'):
+                username = split_msg[1] # (' '.join(message.split(' ')[1:]))
+                print(split_msg)
+                print(username,'res')
+                
+                if(username in self.usernames):  
+                    client.send("ERRO Já existe um usuário com esse nome".encode('utf-8')) 
+                    #return
+
+                elif(len(split_msg) > 2):
+                    client.send("ERRO Nomes não podem ter espaços".encode('utf-8'))
+                    #return
+
+                else:     
+                    self.usernames.append(username)
+                    self.clients.append(client)
+
+                    print(f"username do cliente é {username}!")
+                    self.broadcast(f"{username} entrou no chat!".encode('utf-8'))
+                    client.send('REGISTRO_OK'.encode('utf-8'))
+                    print("regok")
+                    thread = threading.Thread(target=self.handle, args=(client,))
+                    thread.start()
+
+                    client.send('REGISTRO_OK'.encode('utf-8'))
+
 
 if __name__ == "__main__":
     get_host_ip()
     servidor = Servidor()
-    servidor.receive()
+    servidor.register()
 
+    servidor.server.close()
